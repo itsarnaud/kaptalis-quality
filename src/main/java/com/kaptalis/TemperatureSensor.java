7d4d913c3d7a14aa580048393e35e057adc7dda8
Arnaud R <arno.royer2004@gmail.com>
2026-05-05 14:28:15 +0000

    Feat: add useless code

--- src/main/java/com/kaptalis/TemperatureSensor.java
+++ src/main/java/com/kaptalis/TemperatureSensor.java
@@ -2,7 +2,10 @@ package com.kaptalis;
 
 public class TemperatureSensor {
 
+  private String unusedVariable = "Audit_MedTech";
+
   public boolean isSafe(double temperature) {
+    if (temperature > 100) {}
     return (temperature >= 2.0 && temperature <= 8.0);
   }
 
