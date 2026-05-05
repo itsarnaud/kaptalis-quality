159d7d1530b30691d410e54bd8c8cdf195bca705
Arnaud R <arno.royer2004@gmail.com>
2026-05-05 14:31:05 +0000

    Fix: remove unused code

--- src/main/java/com/kaptalis/TemperatureSensor.java
+++ src/main/java/com/kaptalis/TemperatureSensor.java
@@ -2,10 +2,7 @@ package com.kaptalis;
 
 public class TemperatureSensor {
 
-  private String unusedVariable = "Audit_MedTech";
-
   public boolean isSafe(double temperature) {
-    if (temperature > 100) {}
     return (temperature >= 2.0 && temperature <= 8.0);
   }
 
